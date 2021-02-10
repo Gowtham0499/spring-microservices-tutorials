691cb1585c8aa2c30ecfc7a1391b6bd9ab0b6f55
Gowtham0499 <gowth0499@gmail.com>
2021-02-10 10:51:40 +0530

    Completed Currency Conversion Service by Consuming Currency Exchange Service

---
+++ currency-conversion-service/src/main/java/com/currency/conversion/service/app/CurrencyConversionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.currency.conversion.service.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+
+}
---
+++ currency-conversion-service/src/main/java/com/currency/conversion/service/app/controllers/CurrencyConversionController.java
@@ -0,0 +1,30 @@
+package com.currency.conversion.service.app.controllers;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.currency.conversion.service.app.model.CurrencyConversion;
+
+@RestController
+public class CurrencyConversionController {
+	
+	@GetMapping(value = "/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversion convertCurrency(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
+		
+		HashMap<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("from", from);
+		uriVariables.put("to", to);
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<CurrencyConversion> responseEntity = restTemplate.getForEntity("http://localhost:8000/forex-rates/from/{from}/to/{to}", CurrencyConversion.class, uriVariables);
+		CurrencyConversion currencyConversion = responseEntity.getBody();
+		return new CurrencyConversion(currencyConversion.getId(), from, to, quantity, currencyConversion.getForexExchangeRateValue(), quantity.multiply(currencyConversion.getForexExchangeRateValue()), currencyConversion.getEnvironment());
+		
+	}
+
+}
